44d0382f493fe5432f4f9c39c7faa7173c5e9374
Mark Tasaka <mark.tasaka@gmail.com>
2024-10-10 15:07:49 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header";
+import { Assignments } from "./components/Assignments";
+
+//Mark Tasaka
+//Lab 3
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Assignments />
+    </>
+  );
+}
+
+export default App;
